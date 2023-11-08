c96f6e1636b03e258200ea4d2e02457ce54b9159
Connor McSeveney <cjmc7e@berkeley.edu>
2023-11-08 15:22:19 -0800

    vite initialized, searchbar made

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-hello!
+Current state (last update: Connor 11/8 15:22):
+npm/React projects initialized, and can be run (use npm run dev in /website/vite-project)
+main.jsx is the current front-end hub, I'm working on a search bar to center the site around.
---
+++ vite-project/src/SearchBar.jsx
@@ -0,0 +1,20 @@
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+
+export default function SearchBar() {
+  return (
+    <Form inline>
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+            />
+          </Col>
+          <Col xs="auto">
+            <Button type="submit">Submit</Button>
+          </Col>
+        </Row>
+      </Form>
+  )
+}
