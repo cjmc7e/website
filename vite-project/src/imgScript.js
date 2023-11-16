26df6cc370da0a13bd2681e5452f5a36120d98f3
Parth Patel <parth773@berkeley.edu>
2023-11-15 18:10:53 -0800

    added img prototype

---
+++ vite-project/src/imgScript.js
@@ -0,0 +1,29 @@
+function imageGen(album = null) {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = document.getElementById("back");
+    //const cover = document.getElementById("cover")
+
+    const imageWidth = img.width;
+    const imageHeight = img.height;
+    const coverw = cover.width;
+    const coverh = cover.height;
+    // rescaling the canvas element
+    canvas.width = imageWidth;
+    canvas.height = imageHeight;
+    ctx.drawImage(img, 0, 0, imageWidth, imageHeight);
+    ctx.drawImage(cover, 195, 270, coverw*2.1, coverh*2.1);
+    ctx.font = '120.333px Arial';
+    ctx.fillText("Black Country, New Road", 650.56, 2452.24);
+    ctx.fillText("Ants from up there", 1711.27, 2615.88);
+    ctx.font = '50.333px Arial';
+    const text = "1 INTRO 2 CHAOS SPACE MARINE 3 CONCORDE";
+    const text1 = "4 BREAD SONG 5 GOOD WILL HUNTING 6 HALDERN 7 MARK'S THEME";
+    const text2 = "8 THE PLACE WHERE HE INSERTED THE BLADE  9 SNOW GLOBES 10 BASKETBALL SHOES";
+    ctx.fillText(text, 1000.93, 2856.91);
+    ctx.fillText(text1, 1000.93, 2956.91);
+    ctx.fillText(text2, 1000.93, 2874.91);
+    ctx.font = '50.333px Arial';
+    ctx.fillText("4 Febuary 2022",155.04,3301.39);
+    ctx.fillText("58m 46s",994.25,3301.39);
+}
