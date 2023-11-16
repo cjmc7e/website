06277be2ad409e82c65751a5ec68473d4e28bffa
suesther <esthersuue@gmail.com>
2023-11-15 17:50:59 -0800

    code

---
+++ backend/getCode.js
@@ -0,0 +1,44 @@
+const getLink = async (e) => {
+    var client_id = "685fe7b45b254865ae76e5ef47b00cbf";
+    var client_secret = "6bd5fb3f26784a069b771f68efa0e209";
+
+    var authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      method: "post", // Specify the HTTP method
+      headers: {
+        Authorization: "Basic " + btoa(client_id + ":" + client_secret),
+        "Content-Type": "application/x-www-form-urlencoded", // Specify the content type
+      },
+      data: "grant_type=client_credentials",
+    };
+
+    try {
+      const response = await axios(authOptions); // Use axios to make the POST request
+
+      if (response.status === 200) {
+        var token = response.data.access_token;
+        console.log(`auth success! this is token: ${token}`)
+      }
+
+      const inputValue = e; // Get the input value
+
+      const searchResponse = await fetch(
+        "https://api.spotify.com/v1/albums/" + inputValue,
+        {
+          method: "GET",
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      console.log(`searchResponse: ${searchResponse}`);
+      const data = await searchResponse.json();
+      const uri = data["uri"];
+      const codeLink = "https://scannables/scdn.co/uri/plain/jpeg/000000/white/640/" + uri;
+      console.log(`data: ${data}`);
+      return codelink;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  
+export default SearchBar;
