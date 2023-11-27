014ef1d5f78f06ee22bf1b2f10fb5c209a7d694f
Parth Patel <parth773@berkeley.edu>
2023-11-26 21:26:29 -0800

    genrelized

--- Image Gen/script.js
+++ Image Gen/script.js
@@ -5,18 +5,6 @@ document.fonts.add(Metro);
 document.fonts.add(boldMetro);
 document.fonts.add(semiboldMetro);
 var pos = 1
-function track(lst,ctx){
-for(const i=0;i > lst.length; i++){
-    ctx.font = 'bolder .8in Metro'
-    ctx.fillText(i+1, 155, 2750)
-    ctx.font = '.8in Metro'
-    ctx.fillText(lst[i], 205, 2750, );
-    console.log(Metro);
-
-      // Move the text position for the next item
-    ctx.translate(50, 0);
-}
-}
 function upper(lst){
     for (let i = 0; i < lst.length; i++) {
         lst[i]= lst[i].toUpperCase()
@@ -55,13 +43,10 @@ function widther(l,ctx){
         } 
 }
 
-function imageGen(album = null) {
+function imageGen(cover,artist,album,color,tracks,rd,al,code) {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     const img = document.getElementById("back");
-    const cover = document.getElementById("cover");
-    const color = document.getElementById("pallete");
-    const code = document.getElementById("code");
     let y_lim = 3100
 
     const imageWidth = img.width;
@@ -72,20 +57,19 @@ function imageGen(album = null) {
     
     canvas.height = imageHeight;
     ctx.drawImage(img, 0, 0, imageWidth, imageHeight);
-    ctx.drawImage(cover, 162, 112, coverw*2.1, coverh*2.1);
+    ctx.drawImage(cover, 162, 112, coverw*2.1, coverh*2.1); //cover needs to be 2100x2100
     ctx.textAlign = 'left';
     ctx.font = '500 3.5in Metro';
-    ctx.fillText("Black Country, New Road", 165, 2470, 2100);
+    ctx.fillText(artist, 165, 2470, 2100);
     ctx.font = '500 2.2in Metro';
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 15.6;
     ctx.beginPath();
     ctx.moveTo(159, 2545);//2545
     ctx.lineTo(2265,2545);//2545
-    ctx.drawImage(color, 159, 2560, color.width, color.height)
-    ctx.fillText("Funeral", 1400, 2711, 1000);
-    const lst = ["Give Life Back to Music", "The Game of Love", "Giorgio by Moroder", "Within", "Instant Crush", "Lose Yourself to Dance", "Touch", "Get Lucky", "Beyond", "Motherboard", "Fragments of Time", "Doin' It Right", "Contact"]//,"Give Life Back to Music", "The Game of Love", "Giorgio by Moroder", "Within", "Instant Crush", "Lose Yourself to Dance", "Touch", "Get Lucky", "Beyond", "Motherboard", "Fragments of Time", "Doin' It Right", "Contact"];
-    const l = upper(lst)
+    ctx.drawImage(color, 159, 2560, color.width, color.height) //pallete needs to be 1185x144
+    ctx.fillText(album, 1400, 2711, 1000);
+    const l = upper(tracks)
     let x = 155
     let y = 2795
     ctx.font = '400 .65in Metro'
@@ -103,7 +87,6 @@ function imageGen(album = null) {
           x += ctx.measureText((pos-1) + ' ' + w[i]).width+30
         }
         if(l.length == 0){
-          console.log('WE UP but confused')
           ctx.stroke();
         ctx.beginPath();
         ctx.moveTo(159, y+30);
@@ -130,9 +113,8 @@ function imageGen(album = null) {
         ctx.fillText('Release Date',136,3275);
         ctx.fillText('Album Length',897,3275);
         ctx.font = '500 1in Metro';
-        ctx.fillText('Funeral',1963,3275);
+        ctx.fillText(album,1963,3275);
         ctx.font = '400 .65in Metro';
-        ctx.fillText('14 September 2004',136,3345);
-        ctx.fillText('48m 2s',897,3345);
-        ctx.drawImage(code, 1963, 3300, code.width, code.height)
-}
+        ctx.fillText(rd,136,3345);
+        ctx.fillText(al,897,3345);
+        ctx.drawImage(code, 1963, 3300, code.width, code.height) //code needs to be 340x84
