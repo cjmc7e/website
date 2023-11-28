6f4fc82b13f189ce2e2553fc526348531576875b
suesther <esthersuue@gmail.com>
2023-11-28 13:12:21 -0800

    spotify code

--- backend/getCode.js
+++ backend/getCode.js
@@ -32,8 +32,8 @@ const getLink = async (e) => {
 
       console.log(`searchResponse: ${searchResponse}`);
       const data = await searchResponse.json();
-      const uri = data["uri"];
-      const codeLink = "https://scannables/scdn.co/uri/plain/jpeg/000000/white/640/" + uri;
+      const uri = data.uri;
+      const codeLink = "https://scannables.scdn.co/uri/plain/png/000000/white/640/"+ uri;
       console.log(`data: ${data}`);
       return codelink;
     } catch (error) {
@@ -41,4 +41,4 @@ const getLink = async (e) => {
     }
   };
   
-export default SearchBar;
+export default getLink;
