4c0758c021b9e07f180ff4853540a5b302e1b03e
suesther <esthersuue@gmail.com>
2023-11-28 23:12:31 -0800

    image file

--- backend/getCode.js
+++ backend/getCode.js
@@ -35,7 +35,9 @@ const getLink = async (e) => {
       const uri = data.uri;
       const codeLink = "https://scannables.scdn.co/uri/plain/png/000000/white/640/"+ uri;
       // console.log(`data: ${data}`);
-      return codeLink;
+      let img = new Image();
+      img.src = codeLink;
+      return img;
     } catch (error) {
       console.error("Error:", error);
     }
